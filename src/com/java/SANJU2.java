1aa5601839b51ea70fe8273908f1db89bbdf2229
Prink0054 <turna.prink@gmail.com>
2021-05-22 16:17:20 +0530

    2nd commit

---
+++ src/com/java/SANJU2.java
@@ -0,0 +1,9 @@
+package com.java;
+
+public class SANJU2 {
+public static void main(String[] args) {
+	
+	System.out.println("Hello first commit");
+	
+} 
+}
---
+++ src/com/java/SANJU3.java
@@ -0,0 +1,9 @@
+package com.java;
+
+public class SANJU3 {
+public static void main(String[] args) {
+	
+	System.out.println("Hello first commit");
+	
+} 
+}
